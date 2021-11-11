641752248a093828f0a6b9b82a172a8ad7eac8df
oliv-e <oliversmk7rpl@gmail.com>
2021-11-11 12:28:59 +0700

    up seeders/data_awal

--- database/seeders/data_awal.php
+++ database/seeders/data_awal.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class data_awal extends Seeder
 {
@@ -13,6 +14,13 @@ class data_awal extends Seeder
      */
     public function run()
     {
-        //
+        $storeadmin = new User();
+        $storeadmin->name = 'Admin';
+        $storeadmin->email = 'admin@app.com';
+        $storeadmin->password = bcrypt('12345678');
+        $storeadmin->level = 'admin';
+        $storeadmin->nik = '6171123443211234';
+        $storeadmin->hp = '081234123412';
+        $storeadmin->save();
     }
 }
