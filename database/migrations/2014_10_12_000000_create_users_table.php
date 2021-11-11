6266a55b9679a2aca31a5d18dcc3252b23b0495a
oliv-e <oliversmk7rpl@gmail.com>
2021-11-11 12:47:16 +0700

    update seeders dan migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('nik');
+            $table->string('level');
             $table->string('hp');
             $table->rememberToken();
             $table->timestamps();
